b26872552e0b4637d820afd563ef4db17b46dabf
Howard C <h2chang56@gmail.com>
2020-07-05 17:03:00 -0400

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:munch/login_widget.dart';
 import 'package:munch/service/google_authentication.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,6 +16,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Munch App',
       home: Login(_googleAuthentication),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Munch App'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
     );
   }
 }
