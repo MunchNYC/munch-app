465e7bd8486dcd02c6e4cde56a80d1c4769df8f5
vasa137 <vasa.nikola@gmail.com>
2020-10-01 00:16:01 +0200

    Info dialog localization

--- lib/widget/screen/swipe/include/filters_info_dialog.dart
+++ lib/widget/screen/swipe/include/filters_info_dialog.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:munch/theme/palette.dart';
 import 'package:munch/theme/text_style.dart';
+import 'package:munch/util/app.dart';
 import 'package:munch/util/navigation_helper.dart';
 import 'package:munch/widget/util/custom_button.dart';
 
@@ -19,31 +20,31 @@ class FiltersInfoDialog extends StatelessWidget{
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text("Personal Filters", style: AppTextStyle.style(AppTextStylePattern.heading6, fontWeight: FontWeight.w600)),
+              Text(App.translate("filters_screen.info_dialog.personal_filters.title"), style: AppTextStyle.style(AppTextStylePattern.heading6, fontWeight: FontWeight.w600)),
             ],
           ),
           SizedBox(height: 20.0),
-          Text("Only These", style: AppTextStyle.style(AppTextStylePattern.body2, fontSizeOffset: 1.0, color: Palette.hyperlink, fontWeight: FontWeight.w600)),
-          Text("Restaurant you see will have at least one of these cuisines types.", style: AppTextStyle.style(AppTextStylePattern.body2, fontSizeOffset: 1.0,color: Palette.primary)),
+          Text(App.translate("filters_screen.info_dialog.personal_filters.whitelist.title"), style: AppTextStyle.style(AppTextStylePattern.body2, fontSizeOffset: 1.0, color: Palette.hyperlink, fontWeight: FontWeight.w600)),
+          Text(App.translate("filters_screen.info_dialog.personal_filters.whitelist.description"), style: AppTextStyle.style(AppTextStylePattern.body2, fontSizeOffset: 1.0,color: Palette.primary)),
           SizedBox(height: 16.0),
-          Text("Definitely No", style: AppTextStyle.style(AppTextStylePattern.body2, color: Palette.secondaryDark, fontSizeOffset: 1.0, fontWeight: FontWeight.w600)),
-          Text("Restaurant you see will not include these cuisine types.", style: AppTextStyle.style(AppTextStylePattern.body2, fontSizeOffset: 1.0,color: Palette.primary)),
+          Text(App.translate("filters_screen.info_dialog.personal_filters.blacklist.title"), style: AppTextStyle.style(AppTextStylePattern.body2, color: Palette.secondaryDark, fontSizeOffset: 1.0, fontWeight: FontWeight.w600)),
+          Text(App.translate("filters_screen.info_dialog.personal_filters.blacklist.description"), style: AppTextStyle.style(AppTextStylePattern.body2, fontSizeOffset: 1.0,color: Palette.primary)),
           SizedBox(height: 16.0),
-          Text("I'm Okay With", style: AppTextStyle.style(AppTextStylePattern.body2, color: Palette.secondaryLight, fontSizeOffset: 1.0, fontWeight: FontWeight.w600)),
-          Text("Restaurant you see may or may not include these cuisine types.", style: AppTextStyle.style(AppTextStylePattern.body2, fontSizeOffset: 1.0, color: Palette.primary)),
+          Text(App.translate("filters_screen.info_dialog.personal_filters.neutral.title"), style: AppTextStyle.style(AppTextStylePattern.body2, color: Palette.secondaryLight, fontSizeOffset: 1.0, fontWeight: FontWeight.w600)),
+          Text(App.translate("filters_screen.info_dialog.personal_filters.neutral.description"), style: AppTextStyle.style(AppTextStylePattern.body2, fontSizeOffset: 1.0, color: Palette.primary)),
           SizedBox(height: 16.0),
           Row(
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text("Group Filters", style: AppTextStyle.style(AppTextStylePattern.heading6, fontWeight: FontWeight.w600)),
+              Text(App.translate("filters_screen.info_dialog.group_filters.title"), style: AppTextStyle.style(AppTextStylePattern.heading6, fontWeight: FontWeight.w600)),
             ],
           ),
           SizedBox(height: 16.0),
-          Text("A smart combination of all personal filters within this Munch. Restaurants shown to everyone will be based on these filters.", style: AppTextStyle.style(AppTextStylePattern.body2, fontSizeOffset: 1.0, color: Palette.primary)),
+          Text(App.translate("filters_screen.info_dialog.group_filters.description"), style: AppTextStyle.style(AppTextStylePattern.body2, fontSizeOffset: 1.0, color: Palette.primary)),
           SizedBox(height: 16.0),
-          Text("You can change your votes in Personal tab.", style: AppTextStyle.style(AppTextStylePattern.body2, fontSizeOffset: 1.0, color: Palette.primary)),
+          Text(App.translate("filters_screen.info_dialog.summary"), style: AppTextStyle.style(AppTextStylePattern.body2, fontSizeOffset: 1.0, color: Palette.primary)),
           SizedBox(height: 20.0),
           Row(
             mainAxisSize: MainAxisSize.max,
@@ -59,7 +60,7 @@ class FiltersInfoDialog extends StatelessWidget{
                 padding: EdgeInsets.symmetric(vertical: 10.0),
                 color: Palette.background,
                 textColor: Palette.secondaryDark.withOpacity(0.8),
-                content: Text("Close", style: AppTextStyle.style(AppTextStylePattern.heading6, fontWeight: FontWeight.w600, color: Palette.secondaryDark.withOpacity(0.8))),
+                content: Text(App.translate("filters_screen.info_dialog.close_button.text"), style: AppTextStyle.style(AppTextStylePattern.heading6, fontWeight: FontWeight.w600, color: Palette.secondaryDark.withOpacity(0.8))),
                 onPressedCallback: (){
                   // close dialog
                   NavigationHelper.popRoute(context, rootNavigator: true);
