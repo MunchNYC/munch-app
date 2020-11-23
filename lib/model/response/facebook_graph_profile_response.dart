d53c4aa34b6d85ac305009e936ca356aa4864908
Eric Chang <echang25@hotmail.com>
2020-11-23 18:00:08 -0500

    add default value of empty string to Facebook Login properties

--- lib/model/response/facebook_graph_profile_response.dart
+++ lib/model/response/facebook_graph_profile_response.dart
@@ -3,9 +3,9 @@ import 'package:jaguar_serializer/jaguar_serializer.dart';
 part 'facebook_graph_profile_response.jser.dart';
 
 class FacebookGraphProfileResponse{
-  String name;
-  String email;
-  String photoUrl;
+  String name = "";
+  String email = "";
+  String photoUrl = "";
 
   FacebookGraphProfileResponse({this.name, this.email, this.photoUrl});
 }
