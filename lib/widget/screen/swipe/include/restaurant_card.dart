097248567d69afe7ae9627e0e31ab6c2bee88507
aleks azen <aleksazen@gmail.com>
2020-12-06 13:19:42 -0500

    push code

--- lib/widget/screen/swipe/include/restaurant_card.dart
+++ lib/widget/screen/swipe/include/restaurant_card.dart
@@ -13,7 +13,8 @@ class RestaurantCard extends StatefulWidget {
   Restaurant restaurant;
   MunchBloc munchBloc;
 
-  RestaurantCard(this.restaurant, {this.munchBloc}): super(key: Key(restaurant.id));
+  RestaurantCard(this.restaurant, {this.munchBloc})
+      : super(key: Key(restaurant.id));
 
   // must be saved here, because of Dragging purposes. Otherwise we cannot keep same image as it was when drag started
   int currentCarouselPage = 0;
@@ -22,7 +23,7 @@ class RestaurantCard extends StatefulWidget {
   _RestaurantCardState createState() => _RestaurantCardState();
 }
 
-class _RestaurantCardState extends State<RestaurantCard>{
+class _RestaurantCardState extends State<RestaurantCard> {
   // Must be instantiated here to be always created again when drag starts, otherwise we'll get exceptions because carousel controller won't be instantiated again when we start dragging
   CarouselController _carouselController = CarouselController();
 
@@ -34,147 +35,157 @@ class _RestaurantCardState extends State<RestaurantCard>{
       color: Colors.transparent,
       borderRadius: BorderRadius.circular(16.0),
       child: Column(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          _titleSection(),
-          Expanded(
-            child: _imageCarousel()
-          )
-        ]
-      ),
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            _titleSection(),
+            Expanded(child: _imageCarousel())
+          ]),
     );
   }
 
-  Widget _starsRating(){
-    String afterDecimalPointValue = widget.restaurant.rating.toString().substring(widget.restaurant.rating.toString().indexOf(".") + 1);
-    return Image(image: AssetImage("assets/images/yelp/stars/stars_regular_" + widget.restaurant.rating.floor().toString() + (afterDecimalPointValue == "0" ? "" : "_half") + ".png"), height: 16.0);
+  Widget _starsRating() {
+    String afterDecimalPointValue = widget.restaurant.rating
+        .toString()
+        .substring(widget.restaurant.rating.toString().indexOf(".") + 1);
+    return Image(
+        image: AssetImage("assets/images/yelp/stars/stars_regular_" +
+            widget.restaurant.rating.floor().toString() +
+            (afterDecimalPointValue == "0" ? "" : "_half") +
+            ".png"),
+        height: 16.0);
   }
 
-  Widget _yelpStatsRow(){
+  Widget _yelpStatsRow() {
     return GestureDetector(
-        onTap: (){
+        onTap: () {
           Utility.launchUrl(context, widget.restaurant.url);
         },
         child: Row(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Image(image: AssetImage("assets/images/yelp/yelp-burst.png"), height: 16.0),
+            Image(
+                image: AssetImage("assets/images/yelp/yelp-burst.png"),
+                height: 16.0),
             SizedBox(width: 8.0),
             _starsRating(),
             SizedBox(width: 8.0),
-            Text(widget.restaurant.reviewsNumber.toString() + ' ' + App.translate('restaurant_swipe_screen.restaurant_card.yelp.reviews.text'), style: AppTextStyle.style(AppTextStylePattern.body2, color: Palette.secondaryLight)),
+            Text(
+                widget.restaurant.reviewsNumber.toString() +
+                    ' ' +
+                    App.translate(
+                        'restaurant_swipe_screen.restaurant_card.yelp.reviews.text'),
+                style: AppTextStyle.style(AppTextStylePattern.body2,
+                    color: Palette.secondaryLight)),
           ],
-        )
-    );
+        ));
   }
 
-  Widget _titleSection(){
+  Widget _titleSection() {
     // Just check does this restaurant has info for current day, otherwise we'll not show them below
-    bool hasCurrentWorkingHours = widget.restaurant.workingHours != null && widget.restaurant.workingHours.length > 0;
+    bool hasCurrentWorkingHours = widget.restaurant.workingHours != null &&
+        widget.restaurant.workingHours.length > 0;
 
     return Container(
-      padding: EdgeInsets.only(top: 24.0, bottom: 16.0, left: 16.0, right: 16.0),
-      decoration: BoxDecoration(
-        color: Palette.background,
-        borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(16.0),
-            topRight: Radius.circular(16.0)
-        )
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(widget.restaurant.name, style: AppTextStyle.style(AppTextStylePattern.heading3, fontWeight: FontWeight.w500), maxLines: 2, overflow: TextOverflow.ellipsis),
-          SizedBox(height: 8.0),
-          _yelpStatsRow(),
-          SizedBox(height: 8.0),
-          Text((widget.restaurant.priceSymbol != null ? widget.restaurant.priceSymbol + ' • ' : "") + widget.restaurant.categoryTitles, style: AppTextStyle.style(AppTextStylePattern.body2, color: Palette.primary.withOpacity(0.7))),
-          if(hasCurrentWorkingHours)
-          SizedBox(height: 8.0),
-          if(hasCurrentWorkingHours)
-          Text(widget.restaurant.getWorkingHoursCurrentStatus(), style: AppTextStyle.style(AppTextStylePattern.body2, color: Palette.secondaryLight)),
-        ],
-      )
-    );
+        padding:
+            EdgeInsets.only(top: 24.0, bottom: 16.0, left: 16.0, right: 16.0),
+        decoration: BoxDecoration(
+            color: Palette.background,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(16.0),
+                topRight: Radius.circular(16.0))),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(widget.restaurant.name,
+                style: AppTextStyle.style(AppTextStylePattern.heading3,
+                    fontWeight: FontWeight.w500),
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis),
+            SizedBox(height: 8.0),
+            _yelpStatsRow(),
+            SizedBox(height: 8.0),
+            Text(
+                (widget.restaurant.priceSymbol != null
+                        ? widget.restaurant.priceSymbol + ' • '
+                        : "") +
+                    widget.restaurant.categoryTitles,
+                style: AppTextStyle.style(AppTextStylePattern.body2,
+                    color: Palette.primary.withOpacity(0.7))),
+            if (hasCurrentWorkingHours) SizedBox(height: 8.0),
+            if (hasCurrentWorkingHours)
+              Text(widget.restaurant.getWorkingHoursCurrentStatus(),
+                  style: AppTextStyle.style(AppTextStylePattern.body2,
+                      color: Palette.secondaryLight)),
+          ],
+        ));
   }
 
-  Widget _imageCarousel(){
+  Widget _imageCarousel() {
     return Container(
-     width: double.infinity,
-     decoration: BoxDecoration(
-         color: Palette.background, // this is duplicated in order to show background while we're  waiting for image to be rendered
-         borderRadius: BorderRadius.only(
-             bottomLeft: Radius.circular(16.0),
-             bottomRight: Radius.circular(16.0)
-         )
-     ),
-     child: ClipRRect(
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(16.0),
-          bottomRight: Radius.circular(16.0)
-        ),
-        child: Stack(
-          children: [
-            CarouselSlider(
-              items: widget.restaurant.photoUrls.map((photoUrl) =>
-                SizedBox(
-                  width: double.infinity,
-                  child: Image(image: NetworkImage(photoUrl), fit: BoxFit.cover),
-                )
-              ).toList(),
-              options: CarouselOptions(
-                height: double.infinity,
-                autoPlay: false,
-                enlargeCenterPage: false,
-                viewportFraction: 1.0,
-                scrollPhysics: NeverScrollableScrollPhysics(),
-                initialPage: widget.currentCarouselPage,
-                enableInfiniteScroll: false
+        width: double.infinity,
+        decoration: BoxDecoration(
+            color: Palette.background,
+            // this is duplicated in order to show background while we're  waiting for image to be rendered
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(16.0),
+                bottomRight: Radius.circular(16.0))),
+        child: ClipRRect(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(16.0),
+                bottomRight: Radius.circular(16.0)),
+            child: Stack(children: [
+              CarouselSlider(
+                items: widget.restaurant.photoUrls
+                    .map((photoUrl) => SizedBox(
+                          width: double.infinity,
+                          child: Image(
+                              image: NetworkImage(photoUrl), fit: BoxFit.cover),
+                        ))
+                    .toList(),
+                options: CarouselOptions(
+                    height: double.infinity,
+                    autoPlay: false,
+                    enlargeCenterPage: false,
+                    viewportFraction: 1.0,
+                    scrollPhysics: NeverScrollableScrollPhysics(),
+                    initialPage: widget.currentCarouselPage,
+                    enableInfiniteScroll: false),
+                carouselController: _carouselController,
               ),
-              carouselController: _carouselController,
-            ),
-            _carouselControlsRow()
-        ]
-      )
-     )
-    );
+              _carouselControlsRow()
+            ])));
   }
 
-  void _onCarouselLeftSideTapped(){
-    if(widget.currentCarouselPage - 1 >= 0){
+  void _onCarouselLeftSideTapped() {
+    if (widget.currentCarouselPage - 1 >= 0) {
       widget.currentCarouselPage--;
       _carouselController.previousPage();
-    } else{
+    } else {
       widget.munchBloc.add(NoMoreImagesCarouselEvent(isLeftSideTapped: true));
     }
   }
 
-  void _onCarouselRightHalfTapped(){
-    if(widget.currentCarouselPage + 1 < widget.restaurant.photoUrls.length){
+  void _onCarouselRightHalfTapped() {
+    if (widget.currentCarouselPage + 1 < widget.restaurant.photoUrls.length) {
       widget.currentCarouselPage++;
       _carouselController.nextPage();
-    } else{
+    } else {
       widget.munchBloc.add(NoMoreImagesCarouselEvent(isLeftSideTapped: false));
     }
   }
 
-  Widget _carouselControlsRow(){
-    return Row(
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          Expanded(child: GestureDetector(
+  Widget _carouselControlsRow() {
+    return Row(mainAxisSize: MainAxisSize.max, children: <Widget>[
+      Expanded(
+          child: GestureDetector(
               child: Container(color: Colors.transparent),
-              onTap: _onCarouselLeftSideTapped
-            )
-          ),
-          Expanded(child: GestureDetector(
+              onTap: _onCarouselLeftSideTapped)),
+      Expanded(
+          child: GestureDetector(
               child: Container(color: Colors.transparent),
-              onTap: _onCarouselRightHalfTapped
-            )
-          ),
-        ]
-    );
+              onTap: _onCarouselRightHalfTapped)),
+    ]);
   }
 }
