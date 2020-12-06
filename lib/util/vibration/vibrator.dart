13b5e104ad697fcfaf3a77b983fd9a266efc5437
aleks azen <aleksazen@gmail.com>
2020-12-06 15:11:01 -0500

    update match vibration and speed up swiping completion/reset

--- lib/util/vibration/vibrator.dart
+++ lib/util/vibration/vibrator.dart
@@ -1,15 +1,15 @@
 import 'package:vibration/vibration.dart';
 
 class Vibrator {
-  static vibrate(int amplitude, {int duration = 250}) {
+  static vibrate({int amplitude = -1, int duration = 250, List<int> pattern = const [], List<int> intensities = const []}) {
     Vibration.hasVibrator().then((value) async {
       if (value) {
         bool hasAmplitudeControl = await Vibration.hasAmplitudeControl();
 
         if (hasAmplitudeControl) {
-          Vibration.vibrate(duration: duration, amplitude: amplitude);
+          Vibration.vibrate(duration: duration, amplitude: amplitude, pattern: pattern, intensities: intensities);
         } else {
-          Vibration.vibrate(duration: duration);
+          Vibration.vibrate(duration: duration, pattern: pattern);
         }
       }
     });
