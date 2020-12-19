7bf3d8ed4ee5c5bef49a5fa5be1eb8de3f612af5
Eric Chang <echang25@hotmail.com>
2020-12-18 23:49:57 -0500

    remove session token from MapsApi layer + convert objects into Places objects

--- lib/api/maps_api.dart
+++ lib/api/maps_api.dart
@@ -1,4 +1,4 @@
-import 'package:uuid/uuid.dart';
+import 'package:munch/google_maps_webservice-0.0.18/places.dart';
 import 'package:munch/model/coordinates.dart';
 import 'package:munch/model/response/autocomplete_responses.dart';
 
@@ -10,34 +10,45 @@ class MapsApi extends Api {
 
   MapsApi() : super(endpointSetPrefix: ENDPOINT_SET_PREFIX, version: API_VERSION);
 
-  Future<AutocompleteResponses> getAutocomplete(String sessionToken, String input, Coordinates coordinates) async {
+  Future<PlacesAutocompleteResponse> getAutocomplete(String sessionToken, String input, Coordinates coordinates) async {
     String postUrl = "/autocomplete";
 
     Map<String, dynamic> body = {
       "sessionToken": sessionToken,
       "coordinates": {
-        "longitude": 12.1234,
-        "latitude": 12.1
+        "longitude": coordinates.longitude,
+        "latitude": coordinates.latitude
       },
       "input": input
     };
+    print("our post body: ");
+    print(body);
 
     var data = await post(postUrl, body);
 
     AutocompleteResponses getAutocompleteResponses = AutocompleteResponsesJsonSerializer().fromMap(data);
+
+    List<Prediction> predictions = [];
+    getAutocompleteResponses.autocompleteResponses.forEach((element) {
+      predictions.add(Prediction(element.placeId, element.displayString));
+    });
+
+    return PlacesAutocompleteResponse(predictions);
   }
 
-  Future<Coordinates> getCoordinates(String sessionToken, String placeId) async {
+  Future<PlacesDetailsResponse> getCoordinates(String sessionToken, String placeId) async {
     String postUrl = "/coordinates";
 
     Map<String, dynamic> body = {
       "sessionToken": sessionToken,
       "placeId": placeId
     };
+    print("our post body: ");
+    print(body);
     var data = await post(postUrl, body);
 
     Coordinates coordinates = CoordinatesJsonSerializer().fromMap(data['coordinates']);
 
-    return coordinates;
+    return PlacesDetailsResponse(coordinates);
   }
 }
