799102823f431c4413c043707ca6c08d37a65b1d
Eric Chang <echang25@hotmail.com>
2021-01-01 17:50:56 -0500

    remove unused import

--- lib/widget/screen/swipe/include/restaurant_card.dart
+++ lib/widget/screen/swipe/include/restaurant_card.dart
@@ -1,7 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/services.dart';
 import 'package:munch/model/munch.dart';
 import 'package:munch/model/restaurant.dart';
 import 'package:munch/model/user.dart';
