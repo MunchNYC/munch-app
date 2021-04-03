7673031e9abc8fc86799260ad8a9395e93c3541e
Eric Chang <echang25@hotmail.com>
2021-04-02 20:33:11 -0400

    update bottom app bar to have center floating action button - same as top right + button

--- lib/widget/screen/home/home_screen.dart
+++ lib/widget/screen/home/home_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:munch/service/munch/munch_bloc.dart';
 import 'package:munch/theme/palette.dart';
-import 'package:munch/theme/text_style.dart';
 import 'package:munch/util/app.dart';
+import 'package:munch/widget/screen/home/include/create_join_dialog.dart';
 import 'package:munch/widget/screen/home/tabs/munches_tab.dart';
 import 'package:munch/widget/screen/home/tabs/profile_tab.dart';
 import 'package:munch/widget/util/app_status_bar.dart';
+import 'package:munch/widget/util/bottom_app_bar.dart';
+import 'package:munch/widget/util/dialog_helper.dart';
 
 class HomeScreen extends StatefulWidget {
   static GlobalKey<NavigatorState> munchesTabNavigator;
@@ -76,48 +78,37 @@ class _HomeScreenState extends State<HomeScreen> {
     return WillPopScope(
         onWillPop: () => _onBackButtonPressed(context),
         child: Scaffold(
-            backgroundColor: Palette.background,
-            extendBodyBehindAppBar: true,
-            resizeToAvoidBottomInset: false,
-            // this needs to be set there as false, in order to support keyboard on create join dialog without overflowing
-            appBar: AppStatusBar.getAppStatusBar(iconBrightness: Brightness.dark),
-            body: IndexedStack(index: _currentIndex, children: _navigators),
-            // new
-            bottomNavigationBar: Container(
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(color: Palette.secondaryLight),
-                ],
+          backgroundColor: Palette.background,
+          extendBodyBehindAppBar: true,
+          resizeToAvoidBottomInset: false,
+          // this needs to be set there as false, in order to support keyboard on create join dialog without overflowing
+          appBar: AppStatusBar.getAppStatusBar(iconBrightness: Brightness.dark),
+          body: IndexedStack(index: _currentIndex, children: _navigators),
+          bottomNavigationBar:  FABBottomAppBar(
+              color: Colors.black,
+              selectedColor: Colors.redAccent,
+              onTabSelected: onTabTapped,
+              items: [
+                FABBottomAppBarItem(
+                    iconData: Icons.restaurant,
+                    label: App.translate('home_screen.bottom_navigation.munches_tab.title')
+                    ),
+                FABBottomAppBarItem(
+                    assetImage: AssetImage("assets/icons/profile.png"),
+                    label: App.translate('home_screen.bottom_navigation.profile_tab.title')
+                ),
+              ],
               ),
-              child: BottomNavigationBar(
-                elevation: 0.0,
-                iconSize: 32.0,
-                selectedFontSize: AppTextStyle.style(AppTextStylePattern.body2, fontSizeOffset: 1.0).fontSize,
-                unselectedFontSize: AppTextStyle.style(AppTextStylePattern.body2, fontSizeOffset: 1.0).fontSize,
-                backgroundColor: Palette.background,
-                selectedItemColor: Palette.primary,
-                unselectedItemColor: Palette.primary,
-                selectedIconTheme: IconThemeData(color: Palette.ternaryDark),
-                unselectedIconTheme: IconThemeData(color: Palette.primary),
-                selectedLabelStyle:
-                    AppTextStyle.style(AppTextStylePattern.body2, fontWeight: FontWeight.w600, fontSizeOffset: 1.0),
-                unselectedLabelStyle: AppTextStyle.style(AppTextStylePattern.body2, fontSizeOffset: 1.0),
-                onTap: onTabTapped,
-                currentIndex: _currentIndex,
-                items: [
-                  BottomNavigationBarItem(
-                    icon: ImageIcon(AssetImage("assets/icons/munchIcon.png"), size: 32.0),
-                    label: App.translate('home_screen.bottom_navigation.munches_tab.title'),
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Padding(
-                        padding: EdgeInsets.only(top: 8.0),
-                        child: ImageIcon(AssetImage("assets/icons/profile.png"), size: 24.0)),
-                    label: App.translate('home_screen.bottom_navigation.profile_tab.title'),
-                  ),
-                ],
-              ),
-            )));
+            floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                DialogHelper(dialogContent: CreateJoinDialog(munchBloc: _munchBloc), rootNavigator: true).show(context);
+              },
+              child: Icon(Icons.add, size: 32.0),
+              elevation: 2.0,
+              backgroundColor: Colors.redAccent,
+            ),
+          ));
   }
 
   void onTabTapped(int index) {
