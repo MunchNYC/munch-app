2a59345dc455dfee62bb1a3d26895305037d1f2e
Eric Chang <echang25@hotmail.com>
2021-06-26 18:11:22 -0400

    committing onboarding screen - skip button broken

---
+++ lib/widget/screen/onboarding/onboarding_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sliding_tutorial/flutter_sliding_tutorial.dart';
+import 'package:munch/widget/screen/onboarding/sliding_tutorial.dart';
+
+import 'package:flutter/widgets.dart';
+import 'package:munch/widget/util/custom_button.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  OnboardingScreen();
+
+  @override
+  _OnboardingScreenState createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  final ValueNotifier<double> notifier = ValueNotifier(0);
+  int pageCount = 3;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Stack(
+            alignment: AlignmentDirectional.center,
+            children: <Widget>[
+              Align(
+                alignment: Alignment.topLeft,
+                child:
+              CustomButton(
+                minWidth: 4000,
+                content: Text("Skip"),
+                color: Colors.red,
+                flat: true,
+              )),
+
+              /// [StatefulWidget] with [PageView] and [AnimatedBackgroundColor].
+              SlidingTutorial(
+                pageCount: pageCount,
+                notifier: notifier,
+              ),
+
+              /// [SlidingIndicator] for [PageView] in [SlidingTutorial].
+              Align(
+                alignment: Alignment(0, 0.6),
+                child: SlidingIndicator(
+                  indicatorCount: pageCount,
+                  notifier: notifier,
+                  activeIndicator: CircleAvatar(child:Icon(Icons.circle, size: 12.0, color: Colors.redAccent), backgroundColor: Colors.transparent),
+                  inActiveIndicator: CircleAvatar(child:Icon(Icons.circle, size: 12.0, color: Colors.grey), backgroundColor: Colors.transparent), //Image(image: AssetImage('assets/images/floatingActionButtonImage.png')),
+                  margin: 8,
+                  sizeIndicator: 12,
+                ),
+              )
+            ],
+          )),
+    );
+  }
+}
